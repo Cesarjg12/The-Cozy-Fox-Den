9265aa61485741de84a8a84b653dae6554760500
cesarjg28 <cesarjg12@yahoo.com>
2023-08-25 16:54:13 -0700

    Finished implementing navigate after login/signup

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -26,7 +26,7 @@ export default class SignUpForm extends Component {
       // method will resolve to the user object included
       // in the payload of the JSON Web Token (JWT)
       const user = await signUp(formData);
-      this.props.setUser(user);
+      this.props.onSuccess();
     } catch {
       // An error occurred
       // Probably due to a duplicate email
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
 
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
+  const navigate = useNavigate();
+
+  const handleAuthSuccess = () => {
+    navigate('/video-list');
+  };
   return (
     <main>
       <h1>AuthPage</h1>
       <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
-      { showSignUp ?
-          <SignUpForm setUser={setUser} />
-          :
-          <LoginForm setUser={setUser} />
-      }
+      {showSignUp ? (
+  <SignUpForm onSuccess={handleAuthSuccess} />
+) : (
+  <LoginForm onSuccess={handleAuthSuccess} setUser={setUser} />
+)}
     </main>
   );
 }
