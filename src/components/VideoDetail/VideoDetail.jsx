f659012915c09d86677f5c98ada3ec97d7b15099
cesarjg28 <cesarjg12@yahoo.com>
2023-08-28 16:39:27 -0700

    Added backgrounds to pages

--- src/components/VideoDetail/VideoDetail.jsx
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 import { useParams } from 'react-router-dom';
-import '../../pages/App/App.css'
+import '../Categories/Categories.css'
 
 const VideoDetail = ({ videos }) => {
   const { videoId } = useParams();
   const video = videos.find(video => video._id === videoId);
-
+console.log('Video', video)
   if (!video) {
     return <div>Video not found.</div>;
   }
@@ -14,7 +14,7 @@ const VideoDetail = ({ videos }) => {
   const videoUrl = `https://www.youtube.com/watch?v=${video.videoUrl}`;
 
   return (
-    <div className={`video-detail-container category-${video.category}`}>
+    <div className={`category-${video.category}`}>
       <h2>{video.title}</h2>
       <YouTube videoId={video.videoUrl} />
     </div>
